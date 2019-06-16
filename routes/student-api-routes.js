f36a017d9b28c20a2149e5684b9acef9b220bcbb
Kai Richardson <kairichardson2000Gmail.com>
2019-06-15 17:08:44 -0700

    heroku fix

---
+++ routes/student-api-routes.js
@@ -0,0 +1,61 @@
+var db = require("../models");
+
+module.exports = function(app) {
+  app.get("/api/students/:id", function(req, res) {
+    // Here we add an "include" property to our options in our findOne query
+    // We set the value to an array of the models we want to include in a left outer join
+    // In this case, just db.Post
+    console.log(db.Students);
+    db.Students.findOne({
+      where: {
+        id: req.params.id
+      }
+    }).then(function(dbStudent) {
+      console.log(dbStudent);
+      res.json(dbStudent);
+    });
+  });
+
+  /* app.get("api/students/:id", function(req, res) {
+    db.Students.findOne({
+      where: {
+        id: req.params.id
+      },
+      include: [db.cipTable]
+    }).then(function(dbStudent) {
+      console.log(dbStudent);
+      res.json(dbStudent);
+    });
+  }); */
+
+  // create method called to create a new user within our db
+  app.post("/api/students", function(req, res) {
+    db.Students.create(req.body).then(function(dbStudent) {
+      res.json(dbStudent);
+      console.log(dbStudent);
+    });
+  });
+
+  app.delete("/api/students/:id", function(req, res) {
+    db.Students.destroy({
+      where: {
+        id: req.params.id
+      }
+    }).then(function(dbStudent) {
+      console.log(dbStudent);
+      res.json(dbStudent);
+    });
+  });
+
+  //update
+  app.put("/api/students/:id", function(req, res) {
+    db.Students.update(req.body, {
+      where: {
+        id: req.params.id
+      }
+    }).then(function(dbStudent) {
+      console.log(dbStudent);
+      res.json(dbStudent);
+    });
+  });
+};
