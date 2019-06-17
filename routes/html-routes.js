c77e517150ff8a528ed94c95659c5adc38d989e4
Ryyder <brucecho82@hotmail.com>
2019-06-17 12:06:45 -0700

    Update html-routes.js

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -16,7 +16,7 @@ module.exports = function(app) {
 
   // // new user route loads user creation page
   app.get("/newuser", function(req, res) {
-    console.log(req.session);
+    console.log(req.session.id);
     /* res.cookie("user" + req.session.passport.user.id); */
     res.sendFile(path.join(__dirname, "../public/newUser.html"));
   });
@@ -24,14 +24,14 @@ module.exports = function(app) {
   // dashboard route loads user info page
   app.get("/dashboard", function(req, res) {
     console.log("i hit 'dashboard'");
-    console.log(req.session);  
+    console.log(req.session.id);  
     /* res.cookie("user" + req.session.passport.user.id); */
     res.sendFile(path.join(__dirname, "../public/dashboard.html"));
   });
   
   // new user route loads user info update page
   app.get("/updateUser", function(req, res) {
-    console.log(req.session);
+    console.log(req.session.id);
     /* res.cookie("user" + req.session.passport.user.id); */
     res.sendFile(path.join(__dirname, "../public/updateUser.html"));
   });
